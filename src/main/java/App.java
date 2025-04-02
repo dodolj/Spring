a9ae63fe15607e74d1a99c021bf6a95f14c9749c
Дима Дроздов <nordost777@bk.ru>
2025-04-02 19:55:26 +0300

    main2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld hello1 = context.getBean(HelloWorld.class);
+        HelloWorld hello2 = context.getBean(HelloWorld.class);
+
+        Cat cat1 = context.getBean(Cat.class);
+        Cat cat2 = context.getBean(Cat.class);
+
+        System.out.println("HelloWorld одинаковые? " + (hello1 == hello2));
+        System.out.println("Cat одинаковые? " + (cat1 == cat2));
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,8 @@
+import lombok.Data;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+@Data
+public class Cat {
+    private String name;
+}
